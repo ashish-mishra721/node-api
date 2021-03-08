a3b8bec4888f691573fc7be28a59be204e0ce786
Ashish Mishra <ashishmishra721@ymail.com>
2021-03-08 14:02:52 +0530

    add user api

--- models/User.js
+++ models/User.js
@@ -1,34 +1,28 @@
 const mongoose = require('mongoose');
 
 const userSechema =  mongoose.Schema (
-    {​​​​​​​​
-first_name: {​​​​​​​​
-type:String,
-required:true
-        }​​​​​​​​,
-last_name: {​​​​​​​​
-type:String,
-required:true
-        }​​​​​​​​,
-email_id:
-        {​​​​​​​​
-type:String,
-require:true
-        }​​​​​​​​,
-contact_no:
-        {​​​​​​​​
-type:Number,
-require:true
-        }​​​​​​​​,
-password: {​​​​​​​​
-type:String,
-require:true
-        }​​​​​​​​,
-date:{​​​​​​​​
-type:Date,
-default:Date.now
-        }​​​​​​​​
-    }​​​​​​​​
+    {
+        first_name:{
+          type: String,
+          required: true
+        },
+        last_name:{
+          type: String,
+          required: true
+        },
+        email_id:{
+          type: String,
+          required: true
+        },
+        contact_number:{
+          type: Number,
+          required: true
+        },
+        password:{
+          type: String,
+          required: true
+        }
+      }
 )
 
 module.exports = mongoose.model('Users', userSechema);
--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,25 @@ var router = express.Router();
 var Users = require('../models/User')
 
 /* GET users listing. */
-router.get('/', async (req, res) => {​​​​​​​​
-const users = awaitUsers.find();
+router.get('/', async (req, res) => {
+  const users = awaitUsers.find();
 res.send(users);
-}​​​​​​​​);
+});
 
-router.post('/', (req, res) => {​​​​​​​​
-const user = Users.new({​​​​​​​​
+router.post('/', (req, res) =>{
+const user = Users.new({
 first_name:req.body.first_name,
 last_name:req.body.last_name,
 contact_number:req.body.contact_number,
 email_id:req.body.email,
 password:req.body.password
-  }​​​​​​​​)
-user.save().then(data=> {​​​​​​​​
+})
+user.save().then(data=> {
 res.send(data);
-  }​​​​​​​​).catch(err=>{​​​​​​​​
+  }).catch(err=>{
 res.send(err);
-  }​​​​​​​​)
-}​​​​​​​​);
+  })
+});
 
 module.exports = router;
 
